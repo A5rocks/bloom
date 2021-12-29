78a46332fbfa6308f6e4c5906c1ad73015ef5320
A5rocks <git@helvetica.moe>
2021-12-29 15:21:49 +0900

    Move to flit

--- bloom/__init__.py
+++ bloom/__init__.py
@@ -1,5 +1,6 @@
 """bloom - a compact library for discord."""
 from . import ll
 
-# should I have a version? users can just use importlib.metadata though.
 __all__ = ('ll',)
+# TODO: remove this when bumping to py3.8 (users can importlib.metadata)
+__version__ = '0.1'
